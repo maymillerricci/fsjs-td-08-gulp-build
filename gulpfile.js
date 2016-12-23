dfa7a4b72304480717eb71b36c29ad2a342114dc
May Miller-Ricci <maymillerricci@gmail.com>
2016-12-22 18:06:57 -0600

    styles task compiles sass, combines all, minifies, adds source map, and copies files to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'),
+    maps = require('gulp-sourcemaps'), 
+    sass = require('gulp-sass'),
+    csso = require('gulp-csso'),
+  rename = require('gulp-rename'),
      del = require('del');
-
-var options = {
-  dist: 'dist'
-}
 
 // gulp scripts:
 // concatenate, minify, copy all js -> all.min.js
@@ -11,12 +11,20 @@ var options = {
 // with source map
 gulp.task('scripts');
 
-// gulp styles:
-// complie scss files -> css,
+// compile scss files -> css
 // concatenate all and minify -> all.min.css
-// -> copy -> dist/styles
 // with source map
-gulp.task('styles');
+// -> copy -> dist/styles
+gulp.task('styles', function() {
+  return gulp.src('sass/global.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(rename('all.min.css'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('sass'))
+    .pipe(gulp.dest('dist/styles'));
+});
 
 // gulp images:
 // optimize size of jpeg & png files
@@ -25,7 +33,7 @@ gulp.task('images');
 
 // delete everything in dist directory
 gulp.task('clean', function() {
-  del(options.dist);
+  del(['dist', 'sass/**.css*']);
 });
 
 // run clean task first, then run scripts, styles, & images tasks
